c791592aefb789bea3b6a266688ce693a6b42b51
Joel Hans <joel.g.hans@gmail.com>
2020-02-28 14:04:08 -0700

    Squash sed into a single line

--- ingest.sh
+++ ingest.sh
@@ -5,11 +5,12 @@
 
 HOME=$PWD
 TMP_DIR="$PWD/ingest/"
-GO_DIR="${TMP_DIR}/collectors/go.d.plugin"
+GO_DIR="${TMP_DIR}collectors/go.d.plugin"
 DOCS_DIR="$PWD/docs/"
 
 # Check if ingest directory already exists. If it doesn't, clone the core repo.
 # Otherwise, just pull the repo instead of cloning it again.
+echo "Ingest and/or sync with Netdata repositories."
 if [ ! -d ${TMP_DIR} ]
 then
   git clone -b frontmatter https://github.com/joelhans/netdata.git ${TMP_DIR}  
@@ -30,6 +31,7 @@ else
 fi
 
 # Sync .md files from ingest directory to documentation directory.
+echo "Sync .md files from ingest directory to documentation directory."
 rsync -a \
   --exclude=".github/" --exclude=".travis/" \
   --exclude="HISTORICAL_CHANGELOG.md" --exclude="DOCUMENTATION.md" \
@@ -40,6 +42,10 @@ rsync -a \
   --delete --delete-excluded \
   ${TMP_DIR} ${DOCS_DIR}
 
-# Make 
+# Strip comments around frontmatter.
+# TODO: Figure out how to remove the extra newline at the end of files after
+# removing the analytics tag.
+echo "Strip comments around frontmatter and GA tags."
+find ${DOCS_DIR} -name '*.md' -exec sed -i -e '/<!--/d;/-->/d;/\[!\[analytics.*/d' {} \;
 
 echo "Done ingesting."
