3806483593dabe699fac82b37252953e5734b6b9
Ian Zamojc <izamojc@zamtools.com>
2020-03-07 00:08:56 -0500

    file writing

--- injest.js
+++ injest.js
@@ -24,12 +24,17 @@ function sanitize(doc) {
 
 // bb951397e6ba4934885c74242d9152183eb58646
 
+async function getRootSha() {
+  const { data: { commit: { sha } } } = await axios.get('https://api.github.com/repos/joelhans/netdata/branches/master')
+  return sha
+}
+
 // TODO: filter pattern
 async function getPages(root, pattern) {
   // TODO: use proper URL building
   const { data } = await axios.get(`${root}?recursive=true`)
 
-  const node = data.tree.find(node => node.path === 'README.md')
+  const node = data.tree.find(node => node.path === 'health/README.md')
 
   // decode base64 content
   const { data: { content: rawDoc } } = await axios.get(node.url)
@@ -44,17 +49,20 @@ const baseDir = 'junk'
 const outDir = path.join(__dirname, baseDir)
 
 async function ingest() {
-  const pages = await getPages('https://api.github.com/repos/joelhans/netdata/git/trees/5434e1b4d1317cad1d87757b7aef2e8093f209fc')
+  const rootSha = await getRootSha()
+  console.log('rootSha', rootSha)
+  const pages = await getPages(`https://api.github.com/repos/joelhans/netdata/git/trees/${rootSha}`)
 
   pages.forEach(async (page) => {
     const fullPath = path.join(outDir, page.meta.path)
     // because the page path may contain additional directories
     const fullDir = path.dirname(fullPath)
     await fs.mkdir(fullDir, { recursive: true })
-    await fs.writeFile(fullPath, page.body)
-  })
 
-  // const sanitized = sanitize(doc)
+    const sanitized = sanitize(page.body)
+
+    await fs.writeFile(fullPath, sanitized)
+  })
 }
 
 ingest()
