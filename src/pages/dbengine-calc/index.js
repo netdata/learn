9a6a9800d0579d8d1d4f2bff79b233372a465559
joelhans <joel.g.hans@gmail.com>
2020-04-06 12:53:35 -0700

    Push out v1

--- src/pages/dbengine-calc/index.js
+++ src/pages/dbengine-calc/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
-import Select from 'react-select';
 import styles from './styles.module.css';
+
+import CodeBlock from '@theme/CodeBlock'
 
 function Wizard() {
   const [state, setState] = React.useState({
@@ -11,13 +12,13 @@ function Wizard() {
     dims: 2000,
     update: 1,
     retention: 14,
-    compression: 56.5,
+    compression: 50,
     pageSize: 32,
-
   })
 
   const [finalDisk, setFinalDisk] = React.useState('')
   const [finalRAM, setFinalRAM] = React.useState('')
+  const [conf, setConf] = React.useState('')
 
   useEffect(() => {
     const nodes = state.master + state.slaves
@@ -37,6 +38,10 @@ function Wizard() {
     setFinalDisk(Math.round(requiredDisk))
     setFinalRAM(Math.round(requiredRAM))
 
+    const confString = String.raw`[global]
+    dbengine disk space = ${Math.round(requiredDisk)}`
+    setConf(confString)
+
   });
 
   const handleChange = (evt) => {
@@ -46,10 +51,6 @@ function Wizard() {
       [evt.target.name]: value
     })
   }
-
-  // function changeBackground(e) {
-  //   e.target.style.background = 'red';
-  // }
 
   return (
     <Layout
@@ -61,54 +62,100 @@ function Wizard() {
             <div className="col col--9">
               <h1 className="hero__title">RAM and disk usage calculator</h1>
               <p>The Agent's database engine uses RAM to store your real-time metrics, then "spills" historical metrics to disk for efficient long-term storage.</p>
-              <p>This calculator helps you determine how much RAM and disk the Agent will use based on how many slave nodes you have, the frequency at which you collect metrics, and <em>how long you can retain your metrics</em>.</p>
-              <p><blockquote>⚠️ This calculator provides an <em>estimate</em> of disk and RAM usage. Real-life usage may vary based on the accuracy of the values you put here, or due to changes in the compression ratio.</blockquote></p>
+              <p>This calculator helps you determine how much RAM and disk the Agent will use based on how long you want to retain your metrics and a few other settings.</p>
+              <p><blockquote>⚠️ This calculator provides an <em>estimate</em> of disk and RAM usage. Real-life usage may vary based on the accuracy of the values you enter below or due to changes in the compression ratio.</blockquote></p>
             </div>
           </div>
         </div>
       </header>
       <main>
         <section className="container">
-          
-          <div className="row">
-            <div className="col col--6">
-              <form className={classnames(styles.calcForm)}>
-                
-                <label htmlFor="retention">How many days do you want to store your metrics?</label>
-                <input type="number" id="retention" name="retention" value={state.retention} onChange={handleChange} />
 
-                <label htmlFor="update">How often, on average, do your Agents collect metrics?</label>
-                <input type="number" id="update" name="update" value={state.update} onChange={handleChange} />
+          <div className={classnames('row', styles.dbCalc)}>
+            <div className={'col col--8'}>
+              <div className={classnames('row', styles.calcRow)}>
+                <div className={classnames('col col--2', styles.calcInput)}>
+                  <input type="number" id="retention" name="retention" value={state.retention} onChange={handleChange} />
+                </div>
+                <div className={classnames('col col--10', styles.calcInstruction)}>
+                  <label htmlFor="retention">How many days do you want to store metrics?</label>
+                </div>
+              </div>
 
-                <label htmlFor="dims">How many dimensions, on average, do your Agents collect?</label>
-                <input type="number" id="dims" name="dims" min="0" value={state.dims} onChange={handleChange} />
+              <div className={classnames('row', styles.calcRow)}>
+                <div className={classnames('col col--2', styles.calcInput)}>
+                  <input type="number" id="update" name="update" value={state.update} onChange={handleChange} />
+                </div>
+                <div className={classnames('col col--10', styles.calcInstruction)}>
+                  <label htmlFor="update">How often, on average, do your Agents collect metrics?</label>
+                  <p>By default, the Agent collects metrics once per second (<code>1</code>). See the <code>update every</code> setting in your <code>netdata.conf</code> file if yours may be different. If you have streaming nodes, use the average of their settings.</p>
+                </div>
+              </div>
 
-                <label htmlFor="slaves">How many slave streaming nodes do you have?</label>
-                <input type="number" id="slaves" name="slaves" min="0" value={state.slaves} onChange={handleChange} />
+              <div className={classnames('row', styles.calcRow)}>
+                <div className={classnames('col col--2', styles.calcInput)}>
+                  <input type="number" id="dims" name="dims" min="0" value={state.dims} onChange={handleChange} />
+                </div>
+                <div className={classnames('col col--10', styles.calcInstruction)}>
+                  <label htmlFor="dims">How many metrics, on average, do your Agents collect?</label>
+                  <p>
+                    To find this value for an Agent, scroll to the bottom of the dashboard to find the number of metrics your Agent collects. If you have streaming nodes, input the average of these values.
+                  </p>
+                  {/* <img 
+                    className='flex'
+                    src='img/dbengine-calc/dbengine_metrics.png'
+                    alt='Where to find the number of collected metrics in the Agent dashboard' 
+                  /> */}
+                </div>
+              </div>
 
-                <label htmlFor="compression">What is your compression savings ratio?</label>
-                <input type="number" id="compression" name="compression" value={state.compression} onChange={handleChange} />
+              <div className={classnames('row', styles.calcRow)}>
+                <div className={classnames('col col--2', styles.calcInput)}>
+                  <input type="number" id="slaves" name="slaves" min="0" value={state.slaves} onChange={handleChange} />
+                </div>
+                <div className={classnames('col col--10', styles.calcInstruction)}>
+                  <label htmlFor="slaves">How many slave streaming nodes do you have?</label>
+                </div>
+              </div>
 
-                <label htmlFor="pageSize">What is your page cache size?</label>
-                <input type="number" id="pageSize" name="pageSize" value={state.pageSize} onChange={handleChange} />
-              </form>
+              <div className={classnames('row', styles.calcRow)}>
+                <div className={classnames('col col--2', styles.calcInput)}>
+                  <input type="number" id="compression" name="compression" value={state.compression} onChange={handleChange} />
+                </div>
+                <div className={classnames('col col--10', styles.calcInstruction)}>
+                  <label htmlFor="compression">What is your compression savings ratio?</label>
+                  <p>To find this value, click on the <strong>Netdata Monitoring</strong> &rarr; <strong>dbengine</strong> items in the right-hand menu and look at the first chart under the <strong>dbengine</strong> section. Or, open your dashboard and use your browser's search feature to find <strong>compression savings ratio</strong>.</p>
+                  <p>Take the best average value </p>
+                  {/* <img 
+                    className={styles.fullWidth}
+                    src='img/dbengine-calc/dbengine_compression.png'
+                    alt='Where to find the compression savings ratio in the Agent dashboard'
+                  /> */}
+                </div>
+              </div>
+
+              <div className={classnames('row', styles.calcRow)}>
+                <div className={classnames('col col--2', styles.calcInput)}>
+                  <input type="number" id="pageSize" name="pageSize" value={state.pageSize} onChange={handleChange} />
+                  
+                </div>
+                <div className={classnames('col col--10', styles.calcInstruction)}>
+                  <label htmlFor="pageSize">What is your page cache size?</label>
+                  <p>Open your <code>netdata.conf</code> file and find the <code>page cache size</code> setting in the <code>[global]</code> section.</p>
+                </div>
+              </div>
             </div>
-            <div class="col col--6">
-              <p>Instructions go here.</p>
+
+            <div className={classnames("col col--4", styles.calcResults)}>
+
+              <p>With the current settings, the Netdata Agent will use {finalDisk} MiB in disk space to store your metrics for {state.retention} days.</p>
+              <p>You should edit your <code>netdata.conf</code> file to change the <code>dbengine disk space</code> setting to <code>dbengine disk space = {finalDisk}</code>.</p>
+              <CodeBlock className={classnames('conf')} language='conf'>{conf}</CodeBlock>
+              <p>The Netdata Agent will also use {finalRAM} MiB of system memory.</p>
+
             </div>
+
           </div>
-
-          <div className="row">
-            <div className={classnames("col col--6", styles.results)}>
-
-              <p></p>
-
-              <input type="number" id="finalDisk" name="finalDisk" value={finalDisk} readOnly />
-              <input type="number" id="finalRAM" name="finalRAM" value={finalRAM} readOnly />
-
-            </div>
-          </div>
-          
         </section>
       </main>
     </Layout>
