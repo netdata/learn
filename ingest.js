853a57dfa36a6226a75fb8032128c49ac07ad50b
joelhans <joel.g.hans@gmail.com>
2020-05-04 06:10:28 -0700

    Ingest go live docs

--- ingest.js
+++ ingest.js
@@ -273,7 +273,7 @@ async function ingest() {
   }
 
   console.log(`Fetching root SHA for 'netdata' repo...`)
-  const rootSha = await getRootSha('netdata', 'master')
+  const rootSha = await getRootSha('netdata', 'docs-go-live')
   console.log(`Fetching nodes from 'netdata' repo...`)
   const nodes = await getNodes(rootSha)
 
