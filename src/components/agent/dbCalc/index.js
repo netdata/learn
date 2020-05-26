4dafc20812b46f7fb41b80e3f817e8e9f09f13ec
Joel Hans <joel.g.hans@gmail.com>
2020-05-26 16:47:41 -0700

    Add tweaks to explain streaming instances

--- src/components/agent/dbCalc/index.js
+++ src/components/agent/dbCalc/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import classnames from 'classnames';
-import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import styles from './styles.module.scss';
 
 import CodeBlock from '@theme/CodeBlock'
@@ -11,7 +11,7 @@ export function Calculator() {
     slaves: 0,
     dims: 2000,
     update: 1,
-    retention: 14,
+    retention: 1,
     compression: 50,
     pageSize: 32,
   })
@@ -86,11 +86,6 @@ export function Calculator() {
             <p>
               To find this value for an Agent, scroll to the bottom of the dashboard to find the number of metrics your Agent collects. If you have streaming nodes, input the average of these values.
             </p>
-            {/* <img 
-              className='flex'
-              src='img/dbengine-calc/dbengine_metrics.png'
-              alt='Where to find the number of collected metrics in the Agent dashboard' 
-            /> */}
           </div>
         </div>
 
@@ -110,11 +105,6 @@ export function Calculator() {
           <div className={classnames('col col--10', styles.calcInstruction)}>
             <label htmlFor="compression">What is your compression savings ratio?</label>
             <p>To find this value, click on the <strong>Netdata Monitoring</strong> &rarr; <strong>dbengine</strong> items in the right-hand menu and look at the first chart under the <strong>dbengine</strong> section. You can also use your browser's search feature to look for <strong>compression savings ratio</strong>. Estimate an average value to input into the calculator.</p>
-            {/* <img 
-              className={styles.fullWidth}
-              src='img/dbengine-calc/dbengine_compression.png'
-              alt='Where to find the compression savings ratio in the Agent dashboard'
-            /> */}
           </div>
         </div>
 
@@ -132,10 +122,20 @@ export function Calculator() {
 
       <div className={classnames("col col--12", styles.calcResults)}>
 
-        <p>With the current settings, the Netdata Agent will use <code>{totalDisk} MiB</code> in disk space to store your metrics for <strong>{state.retention} days</strong>.</p>
-        <p>You should edit your <code>netdata.conf</code> file to change the <code>dbengine disk space</code> setting:</p>
+        <p>Based on the current settings, you should edit the <code>netdata.conf</code> file&nbsp;
+          {state.slaves > 0 &&
+            <em>on your master node&nbsp;</em>
+          }
+          and change the <code>dbengine disk space</code> setting to the following:
+        </p>
         <CodeBlock className={classnames('conf')} language='conf'>{conf}</CodeBlock>
-        <p>The database engine will also use {finalRAM} MiB of system memory, on top of the memory needed to run the rest of the Agent's collection, visualization, and alerting features.</p>
+        <p>Restart your Agent for the setting to take effect.</p>
+        {state.slaves > 0 ? (
+          <p>Your Agent now stores metrics for {state.master + state.slaves} nodes (one master and {state.slaves} slaves) for {state.retention} day{state.retention > 1 && <span>s</span>}. The Agent requires a total of <code>{totalDisk} MiB</code> in disk space on the master node after allocating <code>{diskSetting} MiB</code> to each node (<code>{diskSetting} MiB * 1 master * {state.slaves} slaves = {totalDisk} MiB</code>). See the <Link href="/docs/agent/database/engine">dbengine documentation</Link> for details on how the Agent allocates database engine instances.</p>
+        ) : (
+          <p>Your Agent now stores metrics for {state.retention} day{state.retention > 1 && <span>s</span>} using a total of <code>{totalDisk} MiB</code> in disk space.</p>
+        )}
+        <p>The database engine also uses roughly <code>{finalRAM} MiB</code> of system memory in addition to the memory required for collection, visualization, and alerting features.</p>
 
       </div>
 
