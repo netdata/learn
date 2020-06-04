3f4f2fabe724bfa2874bb298214c4543bc762d50
Joel Hans <joel.g.hans@gmail.com>
2020-06-04 09:11:58 -0700

    Revert ingest, test once more.

--- ingest.js
+++ ingest.js
@@ -24,7 +24,7 @@ const retainPaths = [
 ]
 
 const ax = axios.create({
-  baseURL: 'https://api.github.com/repos/joelhans/',
+  baseURL: 'https://api.github.com/repos/netdata/',
   headers: {
     'Authorization': `token ${GITHUB_TOKEN}`
   }
@@ -285,7 +285,7 @@ async function ingest() {
   }
 
   console.log(`Fetching root SHA for 'netdata' repo...`)
-  const rootSha = await getRootSha('netdata', 'guides-section')
+  const rootSha = await getRootSha('netdata', 'master')
   console.log(`Fetching nodes from 'netdata' repo...`)
   const nodes = await getNodes(rootSha)
 
