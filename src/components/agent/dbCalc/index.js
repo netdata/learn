21b27a72707264065cdbca528f0076745ed51339
Joel Hans <joel.g.hans@gmail.com>
2020-06-11 10:31:44 -0700

    Enforce minimum of 64MiB

--- src/components/agent/dbCalc/index.js
+++ src/components/agent/dbCalc/index.js
@@ -14,8 +14,10 @@ export function Calculator() {
     retention: 1,
     compression: 50,
     pageSize: 32,
+    minDisk: 64,
   })
 
+  const [requiredDisk, setrequiredDisk ] = React.useState('')
   const [totalDisk, setTotalDisk ] = React.useState('')
   const [diskSetting, setDiskSetting] = React.useState('')
   const [finalRAM, setFinalRAM] = React.useState('')
@@ -23,22 +25,29 @@ export function Calculator() {
 
   useEffect(() => {
     const nodes = state.master + state.slaves
-    const totalDims = nodes * state.dims
-    const totalPoints = (totalDims / state.update) * state.retention * 24 * 3600
+    const totalPoints = (state.dims / state.update) * state.retention * 24 * 3600
     const uncompressedPageSize = 4096
     const pointsPerPage = 1024
     const maxPages = totalPoints / pointsPerPage
     const pageSize = uncompressedPageSize / 1024 / 1024
     const uncompressedStorage = maxPages * pageSize
-    const requiredDisk = uncompressedStorage * ( 1 - (state.compression / 100))
-    const diskSetting = requiredDisk / ( nodes )
+
+    // Required disk space for a single DB instance, enforcing a minimum of 64 MiB
+    const nodeDiskSpace = uncompressedStorage * ( 1 - (state.compression / 100))
+    const nodeDiskSpaceMin = nodeDiskSpace < 64 ? 64 : nodeDiskSpace
+
+    // Required disk space for all DB instances
+    const requiredDiskSpace = nodeDiskSpaceMin * nodes
+    setrequiredDisk(Math.round(requiredDiskSpace))
+
+    // Recommended dbengine disk space setting, enforcing a minimum of 64
+    const diskSetting = (requiredDiskSpace / nodes) < 64 ? 64 : (requiredDiskSpace / nodes)
+    setDiskSetting(Math.round(diskSetting))
+
     const cacheRAM = state.pageSize * nodes
-    const dimPages = totalDims * uncompressedPageSize * 2 / 1024 / 1024
+    const dimPages = (nodes * state.dims) * uncompressedPageSize * 2 / 1024 / 1024
     const metadata = uncompressedStorage * 0.03
     const requiredRAM = cacheRAM + dimPages + metadata
-
-    setTotalDisk(Math.round(requiredDisk))
-    setDiskSetting(Math.round(diskSetting))
     setFinalRAM(Math.round(requiredRAM))
 
     const confString = String.raw`[global]
@@ -60,7 +69,7 @@ export function Calculator() {
       <div className={'col col--12'}>
         <div className={classnames('row', styles.calcRow)}>
           <div className={classnames('col col--2', styles.calcInput)}>
-            <input type="number" id="retention" name="retention" value={state.retention} onChange={handleChange} />
+            <input type="number" id="retention" name="retention" value={state.retention} min="0" onChange={handleChange} />
           </div>
           <div className={classnames('col col--10', styles.calcInstruction)}>
             <label htmlFor="retention">How many days do you want to store metrics?</label>
@@ -69,7 +78,7 @@ export function Calculator() {
 
         <div className={classnames('row', styles.calcRow)}>
           <div className={classnames('col col--2', styles.calcInput)}>
-            <input type="number" id="update" name="update" value={state.update} onChange={handleChange} />
+            <input type="number" id="update" name="update" value={state.update} min="1" onChange={handleChange} />
           </div>
           <div className={classnames('col col--10', styles.calcInstruction)}>
             <label htmlFor="update">How often, on average, do your Agents collect metrics?</label>
@@ -79,7 +88,7 @@ export function Calculator() {
 
         <div className={classnames('row', styles.calcRow)}>
           <div className={classnames('col col--2', styles.calcInput)}>
-            <input type="number" id="dims" name="dims" min="0" value={state.dims} onChange={handleChange} />
+            <input type="number" id="dims" name="dims" min="0" value={state.dims} min="0" onChange={handleChange} />
           </div>
           <div className={classnames('col col--10', styles.calcInstruction)}>
             <label htmlFor="dims">How many metrics, on average, do your Agents collect?</label>
@@ -91,7 +100,7 @@ export function Calculator() {
 
         <div className={classnames('row', styles.calcRow)}>
           <div className={classnames('col col--2', styles.calcInput)}>
-            <input type="number" id="slaves" name="slaves" min="0" value={state.slaves} onChange={handleChange} />
+            <input type="number" id="slaves" name="slaves" min="0" step="1" value={state.slaves} min="0" onChange={handleChange} />
           </div>
           <div className={classnames('col col--10', styles.calcInstruction)}>
             <label htmlFor="slaves">How many slave streaming nodes do you have?</label>
@@ -100,7 +109,7 @@ export function Calculator() {
 
         <div className={classnames('row', styles.calcRow)}>
           <div className={classnames('col col--2', styles.calcInput)}>
-            <input type="number" id="compression" name="compression" value={state.compression} onChange={handleChange} />
+            <input type="number" id="compression" name="compression" value={state.compression} min="0" onChange={handleChange} />
           </div>
           <div className={classnames('col col--10', styles.calcInstruction)}>
             <label htmlFor="compression">What is your compression savings ratio?</label>
@@ -110,7 +119,7 @@ export function Calculator() {
 
         <div className={classnames('row', styles.calcRow)}>
           <div className={classnames('col col--2', styles.calcInput)}>
-            <input type="number" id="pageSize" name="pageSize" value={state.pageSize} onChange={handleChange} />
+            <input type="number" id="pageSize" name="pageSize" value={state.pageSize} min="8" onChange={handleChange} />
             
           </div>
           <div className={classnames('col col--10', styles.calcInstruction)}>
@@ -125,7 +134,7 @@ export function Calculator() {
         <div className={styles.calcFinal}>
           <p>With the above configuration, you should allocate the following resources to metrics storage{state.slaves > 0 && <em>&nbsp;on your master node</em>}:</p>
           <span>
-            <code>{totalDisk} MiB</code> in total disk space
+            <code>{requiredDisk} MiB</code> in total disk space
           </span>
           <span>
             <code>{finalRAM} MiB</code> in system memory
@@ -142,22 +151,27 @@ export function Calculator() {
           <CodeBlock className={classnames('conf')} language='conf'>{conf}</CodeBlock>
           <p>Restart your Agent for the setting to take effect.</p>
           {state.slaves > 0 ? (
-            <p>Your Agent now stores metrics for {state.master + state.slaves} nodes (one master and {state.slaves} slave{state.slaves > 1 && <span>s</span>}) for {state.retention} day{state.retention !== 1 && <span>s</span>} using a total of <code>{totalDisk} MiB</code> in disk space.</p>
+            <p>Your Agent now stores metrics for {state.master + state.slaves} nodes (one master and {state.slaves} slave{state.slaves > 1 && <span>s</span>}) for {state.retention} day{state.retention !== 1 && <span>s</span>} using a total of <code>{requiredDisk} MiB</code> in disk space.</p>
           ) : (
-            <p>Your Agent now stores metrics for {state.retention} day{state.retention !== 1 && <span>s</span>} using a total of <code>{totalDisk} MiB</code> in disk space.</p>
+            <p>Your Agent now stores metrics for {state.retention} day{state.retention !== 1 && <span>s</span>} using a total of <code>{requiredDisk} MiB</code> in disk space.</p>
           )}
         </div>
 
         <div className={styles.calcNotes}>
           <h3>Notes</h3>
-          {state.slaves > 0 && (
-            <>
-              <p>Your master node creates separate instances of the database engine for each of your slave nodes, and allocates <code>{diskSetting} MiB</code> to each of them. This is why you must allocate more total disk space than the <code>dbengine disk space</code> setting implies.</p>
-              <p><code>{diskSetting} MiB per instance * 1 master instance * {state.slaves} slave instance{state.slaves > 1 && <span>s</span>} = {totalDisk} MiB</code></p>
-              <p>See the <Link href="/docs/agent/database/engine">dbengine documentation</Link> for details on how the Agent allocates database engine instances.</p>
-            </>
-          )}
-          <p>The system memory figure above is <em>only for the database engine</em>, and it may be higher in real-world situations due to memory fragmentation. The Agent will require more memory for collection, visualization, and alerting features.</p>
+          <ul>
+            {state.slaves > 0 && (
+              <>
+                <li>
+                  <p>Your master node creates separate instances of the database engine for each of your slave nodes, and allocates <code>{diskSetting} MiB</code> to each of them. This is why you must allocate more total disk space than the <code>dbengine disk space</code> setting implies.</p>
+                  <p><code>{diskSetting} MiB per instance * 1 master instance * {state.slaves} slave instance{state.slaves > 1 && <span>s</span>} = {requiredDisk} MiB</code></p>
+                  <p>See the <Link href="/docs/agent/database/engine">dbengine documentation</Link> for details on how the Agent allocates database engine instances.</p>
+                </li>
+              </>
+            )}
+            <li>The database engine requires a minimum of 64 MiB to function (<code>dbengine disk space</code>).</li>
+            <li>The system memory figure above is <em>only for the database engine</em>, and it may be higher in real-world situations due to memory fragmentation. The Agent will require more memory for collection, visualization, and alerting features.</li>
+          </ul>
         </div>
 
       </div>
