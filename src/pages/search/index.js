2805fbedac94487b5c0dcda926794cecea0913a1
Joel Hans <joel.g.hans@gmail.com>
2020-06-11 06:48:59 -0700

    Continuing on search modal

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -24,79 +24,80 @@ function Search() {
       title={`Search Netdata`}
       description="Search all of Netdata's assets from one place: Learn, our blog, GitHub, and more.">
       <main className={classnames('container', styles.searchContainer)}>
-            <div className={classnames('row')}>
+        <div className={classnames('row')}>
               
-                <SearchProvider
-                  config={{
-                    apiConnector: connector
-                  }}
-                >
-                  <WithSearch
-                    mapContextToProps={({ searchTerm, setSearchTerm, results }) => ({
-                      searchTerm,
-                      setSearchTerm,
-                      results
-                    })}
-                  >
-                    {({ searchTerm, setSearchTerm, results }) => {
-                      return (
-                        <>
-                          <div className={classnames('col col--4')}>
-                            <input
-                              className={classnames(styles.searchInput)}
-                              value={searchTerm}
-                              onChange={e => setSearchTerm(e.target.value)}
-                            />
-                            <p>
-                              Your search returned {results.length} queries.                              
-                            </p>
-                            <ResultsPerPage />
-                          </div>
-                          <div className={classnames('col col--8')}>
-                            {results.map(r => (
-                              <div key={r.id.raw} className={classnames(styles.result)}>
-                                {(() => {
-                                  if (r.url.raw.includes('learn.netdata.cloud') == true) {
-                                    return (
-                                      <Link to={r.url.raw.split('https://learn.netdata.cloud')[1]}>
-                                        
-                                        <h3>{r.title.raw} <span className={classnames(styles.resultFlag)}>Learn / Docs</span></h3>
-                                        <p className={classnames(styles.resultUrl)}>{r.url.raw}</p>
-                                      </Link>
-                                    )
-                                  } else if (r.url.raw.includes('netdata.cloud/blog') == true) {
-                                    return (
-                                      <Link href={r.url.raw}>
-                                        <span className={classnames(styles.resultFlag)}>Blog</span>
-                                        <h3>{r.title.raw}</h3>
-                                      </Link>
-                                    )
-                                  } else if (r.url.raw.includes('netdata.cloud') == true) {
-                                    return (
-                                      <Link href={r.url.raw}>
-                                        <span className={classnames(styles.resultFlag)}>Netdata</span>
-                                        <h3>{r.title.raw}</h3>
-                                      </Link>
-                                    )
-                                  } else if (r.url.raw.includes('github.com') == true) {
-                                    return (
-                                      <Link href={r.url.raw}>
-                                        <span className={classnames(styles.resultFlag)}>GitHub</span>
-                                        <h3>{r.title.raw}</h3>
-                                      </Link>
-                                    )
-                                  }
-                                })()}
-                              </div>
-                            ))}
-                          </div>
-                        </>
-                      );
-                    }}
-                  </WithSearch>
-                </SearchProvider>
+          <SearchProvider
+            config={{
+              apiConnector: connector
+            }}
+          >
+            <WithSearch
+              mapContextToProps={({ searchTerm, setSearchTerm, results }) => ({
+                searchTerm,
+                setSearchTerm,
+                results
+              })}
+            >
+              {({ searchTerm, setSearchTerm, results }) => {
+                return (
+                  <>
+                    <div className={classnames('col col--4')}>
+                      <input
+                        className={classnames(styles.searchInput)}
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                        placeholder="Search Netdata Learn..."
+                      />
+                      <p>
+                        Your search returned {results.length} queries.                              
+                      </p>
+                      <ResultsPerPage />
+                    </div>
+                    <div className={classnames('col col--8')}>
+                      {results.map(r => (
+                        <div key={r.id.raw} className={classnames(styles.result)}>
+                          {(() => {
+                            if (r.url.raw.includes('learn.netdata.cloud') == true) {
+                              return (
+                                <Link to={r.url.raw.split('https://learn.netdata.cloud')[1]}>
+                                  
+                                  <h3>{r.title.raw} <span className={classnames(styles.resultFlag)}>Learn / Docs</span></h3>
+                                  <p className={classnames(styles.resultUrl)}>{r.url.raw}</p>
+                                </Link>
+                              )
+                            } else if (r.url.raw.includes('netdata.cloud/blog') == true) {
+                              return (
+                                <Link href={r.url.raw}>
+                                  <span className={classnames(styles.resultFlag)}>Blog</span>
+                                  <h3>{r.title.raw}</h3>
+                                </Link>
+                              )
+                            } else if (r.url.raw.includes('netdata.cloud') == true) {
+                              return (
+                                <Link href={r.url.raw}>
+                                  <span className={classnames(styles.resultFlag)}>Netdata</span>
+                                  <h3>{r.title.raw}</h3>
+                                </Link>
+                              )
+                            } else if (r.url.raw.includes('github.com') == true) {
+                              return (
+                                <Link href={r.url.raw}>
+                                  <span className={classnames(styles.resultFlag)}>GitHub</span>
+                                  <h3>{r.title.raw}</h3>
+                                </Link>
+                              )
+                            }
+                          })()}
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                );
+              }}
+            </WithSearch>
+          </SearchProvider>
 
-            </div>
+        </div>
       </main>
     </Layout>
   );
--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -1,15 +1,15 @@
-import React, {useEffect} from 'react';
+import React, {useState, useCallback} from 'react';
 import {createPortal} from 'react-dom';
 import clsx from 'clsx';
 
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import {useHistory} from '@docusaurus/router';
+import Link from '@docusaurus/Link';
 
 import SiteSearchAPIConnector from "@elastic/search-ui-site-search-connector";
 import { SearchProvider, WithSearch, Results, SearchBox, ResultsPerPage } from "@elastic/react-search-ui";
 
-// import styles from './styles.module.scss';
+import styles from './styles.SearchBar.module.scss';
 
 const connector = new SiteSearchAPIConnector({
   documentType: "page",
@@ -17,12 +17,115 @@ const connector = new SiteSearchAPIConnector({
 });
 
 const SearchBar = (props) => {
+  const history = useHistory();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpen = (evt) => {
+    console.log('Open the modal now, please.')
+    setIsOpen(true);
+  }
+
+  const onClose = useCallback((evt) => {
+    evt.preventDefault();
+    if(evt.target === evt.currentTarget) {
+      console.log('Close the modal now, please.')
+      setIsOpen(false);
+    }
+  }, [setIsOpen]);
 
   return (
-    <input
-      className={clsx()}
-      onClick={onOpen}
-    />
+    <>
+      <button
+        className={classnames(styles.searchButton)}
+        onClick={onOpen}>
+          Search
+      </button>
+
+      {isOpen &&
+        createPortal(
+          <div 
+            className={classnames('searchClose', styles.searchContainer)}
+            onClick={onClose}>
+            <div onClick={null} className={styles.searchModal}>
+              
+              <SearchProvider
+                config={{
+                  apiConnector: connector
+                }}
+              >
+                <WithSearch
+                  mapContextToProps={({ searchTerm, setSearchTerm, results }) => ({
+                    searchTerm,
+                    setSearchTerm,
+                    results
+                  })}
+                >
+                  {({ searchTerm, setSearchTerm, results }) => {
+                    return (
+                      <>
+                        
+                        <div className={classnames('col col--4')}>
+                          <input
+                            className={classnames(styles.searchInput)}
+                            value={searchTerm}
+                            onChange={e => setSearchTerm(e.target.value)}
+                            placeholder="Search Netdata Learn..."
+                          />
+                          <div className={styles.resultVolume}>
+                            <p>Your search returned {results.length} queries.</p>
+                            <ResultsPerPage />
+                          </div>
+                        </div>
+                        <div className={classnames('col col--8')}>
+                          {searchTerm !== '' && results.map(r => (
+                            <div key={r.id.raw} className={classnames(styles.searchResultItem)}>
+                              {(() => {
+                                if (r.url.raw.includes('learn.netdata.cloud') == true) {
+                                  return (
+                                    <Link onClick={onClose} to={r.url.raw.split('https://learn.netdata.cloud')[1]}>
+                                      <h3>{r.title.raw} <span className={classnames(styles.resultFlag)}>Learn / Docs</span></h3>
+                                      <p className={classnames(styles.resultUrl)}>{r.url.raw}</p>
+                                    </Link>
+                                  )
+                                } else if (r.url.raw.includes('netdata.cloud/blog') == true) {
+                                  return (
+                                    <Link href={r.url.raw}>
+                                      <h3>{r.title.raw} <span className={classnames(styles.resultFlag)}>Blog</span></h3>
+                                      <p className={classnames(styles.resultUrl)}>{r.url.raw}</p>
+                                    </Link>
+                                  )
+                                } else if (r.url.raw.includes('netdata.cloud') == true) {
+                                  return (
+                                    <Link href={r.url.raw}>
+                                      <h3>{r.title.raw} <span className={classnames(styles.resultFlag)}>Netdata.Cloud</span></h3>
+                                      <p className={classnames(styles.resultUrl)}>{r.url.raw}</p>
+                                    </Link>
+                                  )
+                                } else if (r.url.raw.includes('github.com') == true) {
+                                  return (
+                                    <Link href={r.url.raw}>
+                                      <h3>{r.title.raw} <span className={classnames(styles.resultFlag)}>GitHub</span></h3>
+                                      <p className={classnames(styles.resultUrl)}>{r.url.raw}</p>
+                                    </Link>
+                                  )
+                                }
+                              })()}
+                            </div>
+                          ))}
+                        </div>
+                      </>
+                    );
+                  }}
+                </WithSearch>
+              </SearchProvider>
+              
+            </div>  
+          </div>,
+          document.body,
+        )
+      }
+
+    </>
   )
 }
 
