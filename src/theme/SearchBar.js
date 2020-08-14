2b408eba903acfd5b18347b487088ee9821af0c0
Joel Hans <joel.g.hans@gmail.com>
2020-08-14 09:08:33 -0700

    Attempt to lazy load search UI

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -1,22 +1,12 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, {useEffect, useState, useCallback, Suspense, lazy} from 'react';
 import {createPortal} from 'react-dom';
 import clsx from 'clsx';
-import SVG from 'react-inlinesvg';
 
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import {useHistory} from '@docusaurus/router';
 import Link from '@docusaurus/Link';
 
-import SiteSearchAPIConnector from "@elastic/search-ui-site-search-connector";
-import { SearchProvider, WithSearch, Results, SearchBox, ResultsPerPage, Paging, PagingInfo } from "@elastic/react-search-ui";
-
-// import "@elastic/react-search-ui-views/lib/styles/styles.css";
 import styles from './styles.SearchBar.module.scss';
 
-const connector = new SiteSearchAPIConnector({
-  documentType: "page",
-  engineKey: "BZL_aEiLAebVKkcm3eFr"
-});
+const SearchUI = lazy(() => import('./SearchUI/index.js'));
 
 const SearchBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -49,13 +39,6 @@ const SearchBar = (props) => {
     document.body.classList.add('search-open');
   }
 
-  const onClose = useCallback((e) => {
-    if (e.target === e.currentTarget || e.currentTarget.tagName === 'A') {
-      setIsOpen(false);
-      document.body.classList.remove('search-open');
-    }
-  }, [setIsOpen]);
-
   return (
     <>
       <button
@@ -65,105 +48,9 @@ const SearchBar = (props) => {
           <span className={styles.searchKey}>?</span>
       </button>
 
-      {isOpen &&
-        createPortal(
-          <div 
-            className={clsx('searchClose', styles.searchContainer)}
-            onMouseDown={onClose}>
-            <div onClick={null} className={styles.searchModal}>
-              
-              <SearchProvider
-                config={{
-                  apiConnector: connector,
-                  initialState: {
-                    resultsPerPage: 20
-                  }
-                }}
-                searchAsYouType={false}
-              >
-
-                <WithSearch mapContextToProps={({searchTerm, results}) => ({searchTerm, results})}>
-                  {({searchTerm, results}) => {
-                    return (
-                      <>
-                        <header className={styles.searchHeader}>
-                          <SearchBox 
-                            inputProps={{ placeholder: "Search all of Netdata", autoFocus: true }} 
-                            autocompleteResults={{
-                              titleField: "title",
-                              urlField: "url"
-                            }}
-                          />
-                          <div className={styles.resultVolume}>
-                            <PagingInfo />
-                            <ResultsPerPage className={styles.resultPaged} />
-                          </div>
-                        </header>
-                        <div className={styles.searchResults}>
-                          {results.map(r => (
-                            <div key={r.id.raw} className={clsx(styles.searchResultItem)}>
-                              {(() => {
-                                if (r.url.raw.includes('learn.netdata.cloud') == true) {
-                                  return (
-                                    <Link onClick={onClose} to={r.url.raw.split('https://learn.netdata.cloud')[1]}>
-                                      <h3>{r.title.raw} <span className={clsx(styles.resultFlag)}>Learn / Docs</span></h3>
-                                      <p className={clsx(styles.resultUrl)}>{r.url.raw}</p>
-                                    </Link>
-                                  )
-                                } else if (r.url.raw.includes('netdata.cloud/blog') == true) {
-                                  return (
-                                    <Link href={r.url.raw}>
-                                      <h3>{r.title.raw} <span className={clsx(styles.resultFlag)}>Blog</span></h3>
-                                      <p className={clsx(styles.resultUrl)}>{r.url.raw}</p>
-                                    </Link>
-                                  )
-                                } else if (r.url.raw.includes('netdata.cloud') == true) {
-                                  return (
-                                    <Link href={r.url.raw}>
-                                      <h3>{r.title.raw} <span className={clsx(styles.resultFlag)}>Netdata.Cloud</span></h3>
-                                      <p className={clsx(styles.resultUrl)}>{r.url.raw}</p>
-                                    </Link>
-                                  )
-                                } else if (r.url.raw.includes('github.com') == true) {
-                                  return (
-                                    <Link href={r.url.raw}>
-                                      <h3>{r.title.raw} <span className={clsx(styles.resultFlag)}>GitHub</span></h3>
-                                      <p className={clsx(styles.resultUrl)}>{r.url.raw}</p>
-                                    </Link>
-                                  )
-                                }
-                              })()}
-                            </div>
-                          ))}
-
-                          <Paging />
-                        </div>
-                        <footer className={styles.searchFooter}>
-                          <div className={styles.closeInst}>
-                            <div className={styles.closeKey}>
-                              Press <code>Esc</code> to close
-                            </div>
-                            <div className={styles.closeButton}>
-                              <button 
-                                className={clsx('button button--secondary button--lg')} 
-                                onClick={onClose}>
-                                Close
-                              </button>
-                            </div>
-                          </div>
-                        </footer>
-                      </>
-                    );
-                  }}
-                </WithSearch>
-              </SearchProvider>
-              
-            </div>  
-          </div>,
-          document.body,
-        )
-      }
-
+      <Suspense fallback={<div>Loading...</div>}>
+        {isOpen && <SearchUI />}
+      </Suspense>
     </>
   )
 }
