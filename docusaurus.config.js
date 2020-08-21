2cb141c01c56db70acf3cdf67da69b91d509b708
Joel Hans <joel@netdata.cloud>
2020-08-21 09:21:23 -0700

    Re-enable broken link checking

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'netdata',
   projectName: 'netdata',
-  onBrokenLinks: 'warn',
+  onBrokenLinks: 'error',
   presets: [
     [
       '@docusaurus/preset-classic',
