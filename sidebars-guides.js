e30bd533371330d5fed8c2abdf34d643c4543162
Joel Hans <joel.g.hans@gmail.com>
2020-10-01 11:27:19 -0700

    Fix up header and make some cool progress

--- sidebars-guides.js
+++ sidebars-guides.js
@@ -5,22 +5,6 @@ module.exports = {
       label: 'Guides',
       collapsed: false,
       items: [
-        {
-          type: 'category',
-          label: 'Collect & monitor',
-          collapsed: true,
-          items: [
-            'monitor/kubernetes-k8s-netdata',
-            'collect-apache-nginx-web-logs',
-            'monitor/pi-hole-raspberry-pi',
-            'monitor-cockroachdb',
-            'collect-unbound-metrics',
-            'monitor-hadoop-cluster'
-          ]
-        },
-        'using-host-labels',
-        'troubleshoot/monitor-debug-applications-ebpf',
-        'export/export-netdata-metrics-graphite'
       ]
     }
   ]
