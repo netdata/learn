7205684439d0ef17a2380a3810f4793289537a29
Joel Hans <joel.g.hans@gmail.com>
2021-04-26 16:47:55 -0700

    Update news

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,6 +29,17 @@ export const IndexBox = ({children, href, title}) => (
 
 const updates = [
   {
+    title: <>Z-scores collector</>,
+    href: '/docs/agent/collectors/python.d.plugin/zscores',
+    date: 'April 26, 2021',
+    type: 'Collector',
+    description: (
+      <>
+        Create smoothed, rolling Z-Scores for selected metrics or charts to visualize when they deviate from normal.
+      </>
+    ),
+  },
+  {
     title: <>Develop a custom data collector in Python</>,
     href: '/guides/python-collector',
     date: 'March 24, 2021',
@@ -82,29 +93,7 @@ const updates = [
         Learn how to navigate Netdata's Kubernetes monitoring features for visualizing the health and performance of a Kubernetes cluster with per-second granulrity.
       </>
     ),
-  },
-  {
-    title: <>Kubernetes visualizations</>,
-    href: '/docs/cloud/visualize/kubernetes',
-    date: 'February 24, 2021',
-    type: 'Doc',
-    description: (
-      <>
-        Netdata Cloud features rich, zero-configuration Kubernetes monitoring for the resource utilization and application metrics of Kubernetes (k8s) clusters.
-      </>
-    ),
-  },
-  {
-    title: <>Netdata management and configuration cheatsheet</>,
-    href: '/docs/agent/cheatsheet',
-    date: 'February 4, 2021',
-    type: 'Cheatsheet',
-    description: (
-      <>
-        Use our managenment &amp; configuration cheatsheet to simplify your interactions with Netdata, including configuration, using charts, managing the daemon, and more.
-      </>
-    ),
-  },
+  }
 ]
 
 function UpdateBox({title, href, date, type, description}) {
