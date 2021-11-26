ab80892d03730d7d5bd712c62716c63745e7f44d
Tina Lüdtke <tina@kickoke.com>
2021-11-26 10:11:03 +0000

    Changed filename back into interact-new-charts.

--- sidebars.js
+++ sidebars.js
@@ -398,7 +398,7 @@ module.exports = {
             'cloud/visualize/overview',
             'cloud/visualize/nodes',
             'cloud/visualize/dashboards',
-            'cloud/visualize/interact-charts-cloud',
+            'cloud/visualize/interact-new-charts',
             'cloud/visualize/kubernetes',
             
           ]
