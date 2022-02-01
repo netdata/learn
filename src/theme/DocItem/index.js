9338eac42d340774a9a3e9986469a032ad8b6bba
novykh <ioannis.klironomos@gmail.com>
2022-02-01 11:07:51 +0200

    Fixed feedback form.

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -23,6 +23,12 @@ import Link from '@docusaurus/Link';
 
 import { GoThumbsup, GoThumbsdown } from 'react-icons/go';
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 function DocItem(props) {
 	const { content: DocContent } = props;
 	const { metadata, frontMatter } = DocContent;
@@ -40,6 +46,7 @@ function DocItem(props) {
 		formattedLastUpdatedAt,
 		lastUpdatedBy,
 	} = metadata;
+
 	const { pluginId } = useActivePlugin({
 		failfast: true,
 	});
@@ -63,6 +70,20 @@ function DocItem(props) {
 			setFeedback(true);
 		}
 	}, []);
+
+  const [formData, setFormData] = useState({ thumb: null, feedback: "" })
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "thumbs-voting", formData })
+    })
+      .then(() => setFeedback(true))
+      .catch(() => setFeedback(true));
+  };
+
 	// END EDITS
 
 	return (
@@ -129,48 +150,59 @@ function DocItem(props) {
 								{metadata.permalink.includes('/guides/') ? 'guide' : 'document'}{' '}
 								helpful?
 							</p>
-							{feedback && (
+							{feedback ? (
 								<p className="text-lg lg:text-xl text-green-lighter">
 									Thanks for contributing feedback about our docs!
 								</p>
-							)}
-							<form
-								name="thumbs-voting"
-								method="POST"
-								action={`${metadata.permalink}/?success=true`}
-								data-netlify="true"
-								// onsubmit=""
-							>
-								<input type="hidden" form-name="thumbs-voting" name="thumbs" />
-								<button
-									aria-label="Happy"
-									className="group px-4"
-									name="thumbsup"
-								>
-									<GoThumbsup className="w-12 h-12 fill-current text-green-lighter transform transition group-hover:scale-125" />
-								</button>
-								<button
-									aria-label="Unhappy"
-									className="group px-4"
-									name="thumbsdown"
-								>
-									<GoThumbsdown className="w-12 h-12 fill-current text-red transform transition group-hover:scale-125" />
-								</button>
-								{/*Add CSS for feeback text.*/}
-								<label for="feedback-text">
-									Let us know how we can do better:
-								</label>
+							) : (
+                <form
+                  onSubmit={handleSubmit}
+                >
+                  <button
+                    aria-label="Happy"
+                    className="group px-4"
+                    name="thumbsup"
+                    type="button"
+                    onClick={e => setFormData(prevFormData => ({ ...prevFormData, thumb: "up" }))}
+                  >
+                    <GoThumbsup className={`w-12 h-12 fill-current text-green-lighter transform transition group-hover:scale-125 group-active:scale-125 ${formData.thumb === "up" && "scale-125"}`} />
+                  </button>
+                  <button
+                    aria-label="Unhappy"
+                    className="group px-4"
+                    name="thumbsdown"
+                    type="button"
+                    onClick={e => setFormData(prevFormData => ({ ...prevFormData, thumb: "down" }))}
+                  >
+                    <GoThumbsdown className={`w-12 h-12 fill-current text-red transform transition group-hover:scale-125 group-active:scale-125 ${formData.thumb === "down" && "scale-125"}`} />
+                  </button>
 
-								<textarea
-									id="feedback-text"
-									form="doc-feedback"
-									name="feedback"
-									rows="5"
-									cols="50"
-									placeholder="What did you like? What can we improve?"
-								></textarea>
-								<button type="submit">Submit</button>
-							</form>
+                  <div className="mt-4 text-center block">
+                    <label for="feedback-text">
+                      Let us know how we can do better:
+                    </label>
+                  </div>
+                  <div className="mt-4 mb-4 block">
+                    <textarea
+                      className="prose-sm mx-auto p-6 border border-gray-200 rounded dark:bg-gray-800 dark:border-gray-500 w-full"
+                      id="feedback-text"
+                      form="doc-feedback"
+                      name="feedback"
+                      rows="5"
+                      placeholder="What did you like? What can we improve?"
+                      onChange={e => setFormData(prevFormData => ({ ...prevFormData, [e.target.name]: e.target.value }))}
+                    />
+                  </div>
+
+
+                  <button type="submit" disabled={!formData.thumb && !formData.feedback} className="group relative text-text bg-gray-200 px-4 py-2 rounded disabled:bg-gray-100">
+                    <span className="z-10 relative font-semibold group-hover:text-gray-100 group-disabled:text-text">Submit</span>
+                    {(!!formData.thumb || !!formData.feedback) &&
+                      <div className="opacity-0 group-hover:opacity-100 transition absolute z-0 inset-0 bg-gradient-to-r from-green to-green-lighter rounded" />
+                    }
+                  </button>
+                </form>
+              )}
 						</div>
 						{/*Reach out box at the end of the content*/}
 						<div className="markdown prose-sm mt-12 mx-auto p-6 border border-gray-200 rounded shadow-lg dark:bg-gray-800 dark:border-gray-500">
