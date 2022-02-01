26ab3ee1cee106b9ab6eedf1a05813cbbd86a747
novykh <ioannis.klironomos@gmail.com>
2022-02-01 11:07:31 +0200

    Revert "Added Johnny's code for the feedback buttons"
    This reverts commit 9404faed643d3a62f8b4250ca81c05a818841eb8.

--- src/data/News.js
+++ src/data/News.js
@@ -1,274 +1,93 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
- import React, { useState, useEffect } from 'react';
- import DocPaginator from '@theme/DocPaginator';
- import DocVersionSuggestions from '@theme/DocVersionSuggestions';
- import Seo from '@theme/Seo';
- import LastUpdated from '@theme/LastUpdated';
- import TOC from '@theme/TOC';
- import EditThisPage from '@theme/EditThisPage';
- import clsx from 'clsx';
- import styles from './styles.module.css';
- import {
-	 useActivePlugin,
-	 useVersions,
-	 useActiveVersion,
- } from '@theme/hooks/useDocs';
- 
- import Link from '@docusaurus/Link';
- 
- import { GoThumbsup, GoThumbsdown } from 'react-icons/go';
- 
- function DocItem(props) {
-	 const { content: DocContent } = props;
-	 const { metadata, frontMatter } = DocContent;
-	 const {
-		 image,
-		 keywords,
-		 hide_title: hideTitle,
-		 hide_table_of_contents: hideTableOfContents,
-	 } = frontMatter;
-	 const {
-		 description,
-		 title,
-		 editUrl,
-		 lastUpdatedAt,
-		 formattedLastUpdatedAt,
-		 lastUpdatedBy,
-	 } = metadata;
-	 const { pluginId } = useActivePlugin({
-		 failfast: true,
-	 });
-	 const versions = useVersions(pluginId);
-	 const version = useActiveVersion(pluginId); // If site is not versioned or only one version is included
-	 // we don't show the version badge
-	 // See https://github.com/facebook/docusaurus/issues/3362
- 
-	 const showVersionBadge = versions.length > 1; // For meta title, using frontMatter.title in priority over a potential # title found in markdown
-	 // See https://github.com/facebook/docusaurus/issues/4665#issuecomment-825831367
- 
-	 const metaTitle = frontMatter.title || title;
- 
-	 // console.log(metadata)
- 
-	 // BEGIN EDITS
-	 // Netlify Forms: We're using state to figure out whether a user submitted a form yet.
-	 const [feedback, setFeedback] = useState(false);
-	 useEffect(() => {
-		 if (window.location.search.includes('feedback=true')) {
-			 setFeedback(true);
-		 }
-	 }, []);
-	 // END EDITS
- 
-	 return (
-		 <>
-			 <Seo
-				 {...{
-					 title: metaTitle,
-					 description,
-					 keywords,
-					 image,
-				 }}
-			 />
- 
-			 <div className="row">
-				 <div
-					 className={clsx('col', {
-						 [styles.docItemCol]: !hideTableOfContents,
-					 })}
-				 >
-					 <DocVersionSuggestions />
-					 <div className={styles.docItemContainer}>
-						 <article>
-							 {showVersionBadge && (
-								 <div>
-									 <span className="badge badge--secondary">
-										 Version: {version.label}
-									 </span>
-								 </div>
-							 )}
-							 {!hideTitle && (
-								 <header>
-									 <h1 className={styles.docTitle}>{title}</h1>
-								 </header>
-							 )}
- 
-							 {/* BEGIN EDIT */}
-							 {frontMatter.author && (
-								 <aside className="flex items-center mb-12">
-									 <img
-										 src={frontMatter.author_img}
-										 className="w-24 h-24 rounded-full"
-										 alt={frontMatter.author}
-									 />
-									 <div className="ml-4">
-										 <span className="block text-lg lg:text-xl font-medium mb-1">
-											 {frontMatter.author}
-										 </span>
-										 <span className="text-sm font-bold uppercase text-gray-400">
-											 {frontMatter.author_title}
-										 </span>
-									 </div>
-								 </aside>
-							 )}
-							 {/* END EDIT */}
- 
-							 <div className="markdown">
-								 <DocContent />
-							 </div>
-						 </article>
-						 {/* Netlify forms */}
-						 <div className="text-center mt-16 pt-12 border-t border-t-200 dark:border-t-500">
-							 <p className="block text-xl lg:text-2xl font-medium mb-4">
-								 Did you find this{' '}
-								 {metadata.permalink.includes('/guides/') ? 'guide' : 'document'}{' '}
-								 helpful?
-							 </p>
-							 {feedback && (
-								 <p className="text-lg lg:text-xl text-green-lighter">
-									 Thanks for contributing feedback about our docs!
-								 </p>
-							 )}
-							 <form
-								 name="thumbs-voting"
-								 method="POST"
-								 action={`${metadata.permalink}/?success=true`}
-								 data-netlify="true"
-								 // onsubmit=""
-							 >
-								 <input type="hidden" form-name="thumbs-voting" name="thumbs" />
-								 <button
-									 aria-label="Happy"
-									 className="group px-4"
-									 name="thumbsup"
-								 >
-									 <GoThumbsup className="w-12 h-12 fill-current text-green-lighter transform transition group-hover:scale-125" />
-								 </button>
-								 <button
-									 aria-label="Unhappy"
-									 className="group px-4"
-									 name="thumbsdown"
-								 >
-									 <GoThumbsdown className="w-12 h-12 fill-current text-red transform transition group-hover:scale-125" />
-								 </button>
-								 {/*Add CSS for feeback text.*/}
-								 <label for="feedback-text">
-									 Let us know how we can do better:
-								 </label>
- 
-								 <textarea
-									 id="feedback-text"
-									 form="doc-feedback"
-									 name="feedback"
-									 rows="5"
-									 cols="50"
-									 placeholder="What did you like? What can we improve?"
-								 ></textarea>
-								 <button type="submit">Submit</button>
-							 </form>
-						 </div>
-						 {/*Reach out box at the end of the content*/}
-						 <div className="markdown prose-sm mt-12 mx-auto p-6 border border-gray-200 rounded shadow-lg dark:bg-gray-800 dark:border-gray-500">
-							 <h2 className="!text-2xl font-bold !mb-4">Reach out</h2>
-							 <p className="text-sm">
-								 If you need help after reading this{' '}
-								 {metadata.permalink.includes('/guides/') ? 'guide' : 'doc'},
-								 search our{` `}
-								 <Link to="https://community.netdata.cloud">
-									 community forum
-								 </Link>{' '}
-								 for an answer.{` `}
-								 There's a good chance someone else has already found a solution
-								 to the same issue.{` `}
-							 </p>
-							 <div className="flex flex-wrap">
-								 <div className="flex-1">
-									 <h3 className="!mt-0">Documentation</h3>
-									 <ul className="text-sm">
-										 {editUrl && (
-											 <li>
-												 <Link to={editUrl}>Edit</Link> this{' '}
-												 {metadata.permalink.includes('/guides/')
-													 ? 'guide'
-													 : 'doc'}{' '}
-												 directly
-											 </li>
-										 )}
-										 <li>
-											 <Link
-												 to={`https://community.netdata.cloud/new-topic?category_id=6&tags=documentation&title=Feedback%20or%20suggestion%20on:%20${title.replace(
-													 /\s+/g,
-													 '%20'
-												 )}`}
-											 >
-												 Suggest an improvement
-											 </Link>{' '}
-											 for this{' '}
-											 {metadata.permalink.includes('/guides/')
-												 ? 'guide'
-												 : 'doc'}{' '}
-											 in our forum
-										 </li>
-									 </ul>
-								 </div>
-								 <div className="flex-1">
-									 <h3 className="!mt-0">Community</h3>
-									 <ul className="text-sm">
-										 <li>
-											 Join our{' '}
-											 <Link to="https://community.netdata.cloud">
-												 community forum
-											 </Link>
-										 </li>
-										 <li>
-											 Learn how to <Link to="/contribute/">contribute</Link> to
-											 Netdata's open-source
-										 </li>
-										 <li>
-											 Submit a{' '}
-											 <Link to="https://community.netdata.cloud/c/feature-requests/7/">
-												 feature request
-											 </Link>
-										 </li>
-									 </ul>
-								 </div>
-							 </div>
-						 </div>
-						 {/* END EDIT */}
-						 {(editUrl || lastUpdatedAt || lastUpdatedBy) && (
-							 <div className="margin-vert--xl">
-								 <div className="row">
-									 <div className="col">
-										 {editUrl && <EditThisPage editUrl={editUrl} />}
-									 </div>
-									 {(lastUpdatedAt || lastUpdatedBy) && (
-										 <LastUpdated
-											 lastUpdatedAt={lastUpdatedAt}
-											 formattedLastUpdatedAt={formattedLastUpdatedAt}
-											 lastUpdatedBy={lastUpdatedBy}
-										 />
-									 )}
-								 </div>
-							 </div>
-						 )}
-						 <div className="margin-vert--lg">
-							 <DocPaginator metadata={metadata} />
-						 </div>
-					 </div>
-				 </div>
-				 {!hideTableOfContents && DocContent.toc && (
-					 <div className="col col--3">
-						 <TOC toc={DocContent.toc} />
-					 </div>
-				 )}
-			 </div>
-		 </>
-	 );
- }
- 
- export default DocItem;
+import React from 'react';
+
+export const ReleaseVersion = '1.32.0';
+
+export const ReleaseDate = 'November 30, 2021';
+
+export const ReleaseNotes = [
+	'Architectural changes to improve responsiveness, reliability, and stability',
+	'eBPF latency monitoring for disks, IRQs, and more',
+	'ML powered anomaly detection',
+	'eBPF container monitoring using cgroups',
+	'New timezone selector and time control tools',
+];
+
+export const News = [
+	{
+		title: <>Cloud charts 2.0</>,
+		href: 'docs/cloud/visualize/interact-new-charts',
+		date: 'November 30, 2021',
+		type: 'Doc',
+		description: (
+			<>
+				{' '}
+				The improved charts in Netdata Cloud allow you to build the
+				visualizations that meet your needs. You can customize the chart type
+				and move around the chart to catch every metric.
+			</>
+		),
+	},
+	{
+		title: <>Timezone selector is now available on Netdata Cloud</>,
+		href: 'docs/dashboard/visualization-date-and-time-controls#timezone-selector',
+		date: 'September 22, 2021',
+		type: 'Doc',
+		description: (
+			<>
+				With the timezone selector, you have the ability to change the timezone
+				on Netdata Cloud.
+			</>
+		),
+	},
+	{
+		title: <>Date and timeframe controls for visualizations on Netdata Cloud</>,
+		href: 'docs/dashboard/visualization-date-and-time-controls',
+		date: 'August 18, 2021',
+		type: 'Doc',
+		description: (
+			<>
+				Dashboards now allow you to visualize specific timeframes for any time
+				interval either by using a set of quick time-ranges or selecting a
+				custom time-range .
+			</>
+		),
+	},
+	{
+		title: <>Kubernetes monitoring with Netdata: Overview and visualizations</>,
+		href: '/guides/monitor/kubernetes-k8s-netdata',
+		date: 'May 5, 2021',
+		type: 'Guide',
+		description: (
+			<>
+				Learn how to navigate Netdata's Kubernetes monitoring features for
+				visualizing the health and performance of a Kubernetes cluster with
+				per-second granulrity.
+			</>
+		),
+	},
+	{
+		title: <>Z-scores collector</>,
+		href: '/docs/agent/collectors/python.d.plugin/zscores',
+		date: 'April 26, 2021',
+		type: 'Collector',
+		description: (
+			<>
+				Create smoothed, rolling Z-Scores for selected metrics or charts to
+				visualize when they deviate from normal.
+			</>
+		),
+	},
+	{
+		title: <>Develop a custom data collector in Python</>,
+		href: '/guides/python-collector',
+		date: 'March 24, 2021',
+		type: 'Guide',
+		description: (
+			<>
+				Learn how write a custom data collector in Python, which you'll use to
+				collect metrics from and monitor any application that isn't supported
+				out of the box.
+			</>
+		),
+	},
+];
