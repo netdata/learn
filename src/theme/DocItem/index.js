6873d96bd811c0ef721bc3511f5ad5190ea6f81e
Tina Lüdtke <tina@kickoke.com>
2022-02-22 14:11:25 +0000

    Added feedback form

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -4,7 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
 import clsx from 'clsx';
 import DocPaginator from '@theme/DocPaginator';
 import DocVersionBanner from '@theme/DocVersionBanner';
@@ -16,6 +15,48 @@ import TOCCollapsible from '@theme/TOCCollapsible';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import {ThemeClassNames, useWindowSize} from '@docusaurus/theme-common';
+
+// Imports that we need for the custom code:
+import React, { useState, useEffect } from 'react';
+import LastUpdated from '@theme/LastUpdated';
+import EditThisPage from '@theme/EditThisPage';
+import Link from '@docusaurus/Link';
+import { GoThumbsup, GoThumbsdown } from 'react-icons/go';
+
+// Custom constants that we will use in the export function:
+
+// BEGIN EDITS
+	// Netlify Forms: We're using state to figure out whether a user submitted a form yet.
+	const [feedback, setFeedback] = useState(false);
+	useEffect(() => {
+		if (window.location.search.includes('feedback=true')) {
+			setFeedback(true);
+		}
+	}, []);
+
+  const [formData, setFormData] = useState({ thumb: null, feedback: "", url: metadata.permalink })
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { botfield, ...rest } = formData;
+
+    if (botfield) {
+       setFeedback(true)
+       return;
+    }
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "thumbs-voting", ...rest })
+    })
+      .then(() => setFeedback(true))
+      .catch(() => setFeedback(true));
+  };
+
+	// END EDITS
+
+// This function is the source code that renders each documentation page
 export default function DocItem(props) {
   const {content: DocContent} = props;
   const {metadata, frontMatter} = DocContent;
@@ -84,9 +125,125 @@ export default function DocItem(props) {
                   </header>
                 )}
 
+              {/* BEGIN EDIT: Add author documentation if the metadata is supplied */}
+              {frontMatter.author && (
+								<aside className="flex items-center mb-12">
+									<img
+										src={frontMatter.author_img}
+										className="w-24 h-24 rounded-full"
+										alt={frontMatter.author}
+									/>
+									<div className="ml-4">
+										<span className="block text-lg lg:text-xl font-medium mb-1">
+											{frontMatter.author}
+										</span>
+										<span className="text-sm font-bold uppercase text-gray-400">
+											{frontMatter.author_title}
+										</span>
+									</div>
+								</aside>
+							)}
+							{/* END EDIT */}
+
                 <DocContent />
               </div>
 
+{/* BEGIN EDITS: Feedback/ Edit page/ Community boxes */}
+          <div className='markdown'> 
+              {/* BEGIN EDITS: Netlify feedback form*/}
+              <div className="text-center mt-16 pt-12 border-t border-t-200 dark:border-t-500">
+							<p className="block text-xl lg:text-2xl font-medium mb-4">
+								Did you find this{' '}
+								{metadata.permalink.includes('/guides/') ? 'guide' : 'document'}{' '}
+								helpful?
+							</p>
+							{feedback ? (
+								<p className="text-lg lg:text-xl text-green-lighter">
+									Thanks for contributing feedback about our docs!
+								</p>
+							) : (
+                <form
+                  data-netlify="true"
+                  name="thumbs-voting"
+                  method="post"
+                  onSubmit={handleSubmit}
+                >
+                  <input type="hidden" name="url" aria-label="current url" value={formData.url} />
+                  <input type="hidden" name="form-name" value="thumbs-voting" />
+                  <input type="hidden" name="thumb" aria-label="How do you like it?" value={formData.thumb} />
+                  <button
+                    aria-label="Happy"
+                    className="group px-4"
+                    name="thumbsup"
+                    type="button"
+                    onClick={e => setFormData(prevFormData => ({ ...prevFormData, thumb: "Happy" }))}
+                  >
+                    <GoThumbsup className={`w-12 h-12 fill-current text-green-lighter transform transition group-hover:scale-125 group-active:scale-125 ${formData.thumb === "Happy" && "scale-125"}`} />
+                  </button>
+                  <button
+                    aria-label="Unhappy"
+                    className="group px-4"
+                    name="thumbsdown"
+                    type="button"
+                    onClick={e => setFormData(prevFormData => ({ ...prevFormData, thumb: "Unhappy" }))}
+                  >
+                    <GoThumbsdown className={`w-12 h-12 fill-current text-red transform transition group-hover:scale-125 group-active:scale-125 ${formData.thumb === "Unhappy" && "scale-125"}`} />
+                  </button>
+
+                  <div className="mt-4 text-center block">
+                    <label for="feedback-text">
+                      Let us know how we can do better:
+                    </label>
+                  </div>
+                  <div className="mt-4 mb-4 block">
+                    <textarea
+                      className="prose-sm mx-auto p-6 border border-gray-200 rounded dark:bg-gray-800 dark:border-gray-500 w-full"
+                      id="feedback-text"
+                      name="feedback"
+                      rows="5"
+                      placeholder="What did you like? What can we improve?"
+                      onChange={e => setFormData(prevFormData => ({ ...prevFormData, [e.target.name]: e.target.value }))}
+                    />
+                  </div>
+
+
+                  <button type="submit" disabled={!formData.thumb && !formData.feedback} className="group relative text-text bg-gray-200 px-4 py-2 rounded disabled:bg-gray-100">
+                    <span className="z-10 relative font-semibold group-hover:text-gray-100 group-disabled:text-text">Submit</span>
+                    {(!!formData.thumb || !!formData.feedback) &&
+                      <div className="opacity-0 group-hover:opacity-100 transition absolute z-0 inset-0 bg-gradient-to-r from-green to-green-lighter rounded" />
+                    }
+                  </button>
+				  {/*Honeypot to catch spambots*/}
+				  <p class="invisible">
+                    <label>
+                      Don't fill this out if you're human:{' '}
+                      <input
+                        name="botfield"
+                        onChange={e =>
+                          setFormData(prevFormData => ({
+                            ...prevFormData,
+                            [e.target.name]: e.target.value,
+                          }))
+                        }
+                      />
+                    </label>
+                  </p>
+
+                </form>
+
+              ) {/* END EDITS: Netlify feedback form*/}
+
+              {/* BEGIN EDITS: Edit page button */}
+
+              {/* END EDITS: Edit page button*/}
+
+              {/* BEGIN EDITS: Community help */}
+
+              {/* END EDITS: Community help*/}
+
+        </div>
+
+{/* END EDITS: Feedback/ Edit page/ Community boxes */}
               <DocItemFooter {...props} />
             </article>
 
