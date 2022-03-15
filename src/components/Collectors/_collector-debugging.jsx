ad29dc8e47c2d50a40a8dd7ecefdfb48cc0827f0
Tina Lüdtke <tina@kickoke.com>
2022-03-14 17:52:06 -0700

    Added debugging component

---
+++ src/components/Collectors/_collector-debugging.jsx
@@ -0,0 +1,52 @@
+{
+	/* This piece of code generates the configuration section of a collector
+Usage: This file needs to be imported in an MDX file like this: 
+
+<CollectorConfiguration
+	pluginName=""
+	collectorName=""
+/>
+
+pluginName: Assign the plugin name, for example go.d.plugin
+collectorName: Assign the name of the collector, for example activemq
+*/
+}
+import React from 'react';
+import CodeBlock from '@theme/CodeBlock';
+
+export default function CollectorDebug({ pluginName, collectorName }) {
+	return (
+		<>
+			<p>
+				To troubleshoot issues with the <code>{collectorName}</code> collector,
+				run the <code>{pluginName}</code> with the debug option enabled.
+			</p>
+
+			<ol>
+				<li>
+					Navigate to your plugins directory, usually at
+					<code>/usr/libexec/netdata/plugins.d/</code>. If that's not the case
+					on your system, open <code>netdata.conf</code> and look for the
+					setting <code>plugins directory</code>.{' '}
+				</li>
+				<li>
+					In the plugins directory, switch to the <code>netdata</code> user.
+					<CodeBlock className="bash">
+						cd /usr/libexec/netdata/plugins.d/ sudo -u netdata -s
+					</CodeBlock>{' '}
+				</li>
+
+				<li>
+					Run the <code>{pluginName}</code> in debug mode to identify issues:
+					<CodeBlock className="bash">
+						./{pluginName} -d -m {collectorName}
+					</CodeBlock>
+					<p>
+						The output should give you clues as to why the collector isn't
+						working.
+					</p>
+				</li>
+			</ol>
+		</>
+	);
+}
