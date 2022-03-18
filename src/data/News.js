a0ce28cd6c8c911b8f0ad9b87b798f2137cd4d81
Tina Lüdtke <tina@kickoke.com>
2022-03-18 16:41:43 -0700

    Update news on learn

--- src/data/News.js
+++ src/data/News.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const ReleaseVersion = '1.33.0';
+export const ReleaseVersion = '1.33.1';
 
-export const ReleaseDate = 'January 26, 2022';
+export const ReleaseDate = 'February 14, 2022';
 
 export const ReleaseNotes = [
 	'Updated kickstart script to enable automatic installation via DEB or RPM packages wherever available.',
@@ -13,16 +13,31 @@ export const ReleaseNotes = [
 
 export const News = [
 	{
+		title: <>Play pretend: The kickstart script now has a dry-run mode</>,
+		href: '/docs/agent/packaging/installer/update#determine-which-installation-method-you-used',
+		date: 'Mar 16, 2022',
+		type: 'Doc',
+		description: (
+			<>
+				If you have a system that you need to be extra careful on, we have good
+				news for you: When installing or updating Netdata using the kickstart
+				script, you can use the <code>--dry-run</code> option to get a report of
+				what would happen on your node.
+			</>
+		),
+	},
+	{
 		title: <>New version of the kickstart script</>,
 		href: '/docs/agent/packaging/installer/methods/kickstart/',
 		date: 'January 18, 2022',
 		type: 'Doc',
 		description: (
 			<>
-				{' '}
-				Our new and improved one-line install process gets you started with Netdata even faster.
-				Now more portable than ever, it eliminates the need to manually handle dependencies on a majority of Linux systems.
-				Additionally, most users will see greatly reduced resource requirements for the install process compared to the old script.
+				Our new and improved one-line install process gets you started with
+				Netdata even faster. Now more portable than ever, it eliminates the need
+				to manually handle dependencies on a majority of Linux systems.
+				Additionally, most users will see greatly reduced resource requirements
+				for the install process compared to the old script.
 			</>
 		),
 	},
@@ -33,7 +48,6 @@ export const News = [
 		type: 'Doc',
 		description: (
 			<>
-				{' '}
 				The improved charts in Netdata Cloud allow you to build the
 				visualizations that meet your needs. You can customize the chart type
 				and move around the chart to catch every metric.
@@ -75,18 +89,6 @@ export const News = [
 				Learn how to navigate Netdata's Kubernetes monitoring features for
 				visualizing the health and performance of a Kubernetes cluster with
 				per-second granulrity.
-			</>
-		),
-	},
-	{
-		title: <>Z-scores collector</>,
-		href: '/docs/agent/collectors/python.d.plugin/zscores',
-		date: 'April 26, 2021',
-		type: 'Collector',
-		description: (
-			<>
-				Create smoothed, rolling Z-Scores for selected metrics or charts to
-				visualize when they deviate from normal.
 			</>
 		),
 	},
