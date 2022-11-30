18910042e2c79d9d94587b64b4fc2924ca91e0a5
Tasos Katsoulas <tasos@netdata.cloud>
2022-11-30 20:22:07 +0200

    Update learn news (#1241)
    Signed-off-by: Tasos Katsoulas <tasos@netdata.cloud>

--- src/data/News.js
+++ src/data/News.js
@@ -1,19 +1,49 @@
 import React from 'react';
 
-export const ReleaseVersion = '1.36.0';
+export const ReleaseVersion = '1.37.0';
 
-export const ReleaseDate = 'August 11, 2022';
+export const ReleaseDate = 'November 30, 2022';
 
 export const ReleaseNotes = [
-	'New Tiering mechamism to allow for almost unlimited metrics for nodes',
-	'A centralized admin interface',
-	'Metric Correlation improvements',
-	'Anomaly rates on every chart',
+	'Explore the latest changes into your beloved observability tool',
 ];
 
 
 
 export const News = [
+	{
+		title: <>Netdata data source plugin for Grafana</>,
+		href: 'https://www.netdata.cloud/blog/introducing-netdata-source-plugin-for-grafana',
+		date: 'Nov 09, 2022',
+		type: 'Link',
+		description: (
+			<>
+				This initial release of the Netdata data source plugin aims to maximize the troubleshooting capabilities of Netdata in Grafana, making them more widely available. 
+			</>
+		),
+	},
+	{
+		title: <>New Demo space kickoff</>,
+		href: 'https://app.netdata.cloud/spaces/netdata-demo/',
+		date: 'Oct 23, 2022',
+		type: 'Link',
+		description: (
+			<>
+				We have made available multiple rooms in our Demo space for you to experience the power and simplicity of Netdata with live infrastructure monitoring. 
+			</>
+		),
+	},
+	{
+		title: <>A PostgreSQL collector, as it supposed to be</>,
+		href: 'https://blog.netdata.cloud/postgresql-monitoring/',
+		date: 'Sept 26, 2022',
+		type: 'Link',
+		description: (
+			<>
+				Netdata now collects 100+ PostgreSQL metrics and visualizes them across 60+ composite charts.
+			</>
+		),
+	},
 	{
 		title: <>Netdata Agent patch release</>,
 		href: 'https://github.com/netdata/netdata/releases/tag/v1.35.1',
@@ -72,21 +102,4 @@ export const News = [
 			</>
 		),
 	},
-	{
-		title: <>New version of the kickstart script</>,
-		href: '/docs/agent/packaging/installer/methods/kickstart/',
-		date: 'January 18, 2022',
-		type: 'Doc',
-		description: (
-			<>
-				Our new and improved one-line install process gets you started with
-				Netdata even faster. Now more portable than ever, it eliminates the need
-				to manually handle dependencies on a majority of Linux systems.
-				Additionally, most users will see greatly reduced resource requirements
-				for the install process compared to the old script.
-			</>
-		),
-	},
-	
-	
 ];
