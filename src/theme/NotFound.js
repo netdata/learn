c514c28d77d09c378f6e79939a8803bfbeda1802
Fotis Voutsas <fotis@netdata.cloud>
2023-02-20 18:02:28 +0200

    fix

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,10 +1,11 @@
 export default function NotFound() {
-  var url = location.href
-  // var query = url.split('https://learn.netdata.cloud/')[1]
-  var query = url.split('/')[3]
-  var target = query.replaceAll("\/", " ").replace(".", " ")
-  var base = url.split(query)[0]
-  window.location.replace(base + 'search?q=' + target);
-
+  useEffect(() => {
+    window.posthog.capture('page-not-found');
+    var url = location.href
+    var query = url.split('/')[3]
+    var target = query.replaceAll("\/", " ").replace(".", " ")
+    var base = url.split(query)[0]
+    window.location.replace(base + 'search?q=' + target);
+  }, [])
   return null
 }
