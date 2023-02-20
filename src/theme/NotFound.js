b89ae635f7c75df7f7cf7c667fa638ed2cbb196f
Fotis Voutsas <fotis@netdata.cloud>
2023-02-20 18:52:03 +0200

    test

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -6,11 +6,12 @@ import {PageMetadata} from '@docusaurus/theme-common';
 export default function NotFound() {
   useEffect(() => {
     window.posthog.capture('page-not-found');
-    var url = location.href
-    var query = url.split('/')[3]
-    var target = query.replaceAll("\/", " ").replace(".", " ")
-    var base = url.split(query)[0]
-    window.location.replace(base + 'search?q=' + target);
   }, [])
+  var url = location.href
+  var [first, second, three, ...query] = url.split('/')
+
+  var target = query
+  var base = url.split(query)[0]
+  window.location.replace(base + 'search?q=' + 'dummy');
   return null
 }
