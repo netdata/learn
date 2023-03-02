2b052fbdbda3e60765cbe50cc7c7af5c20f5c92b
Tasos Katsoulas <tasos@netdata.cloud>
2023-03-02 14:16:03 +0200

    test
    Signed-off-by: Tasos Katsoulas <tasos@netdata.cloud>

--- ingest/autogenerateRedirects.py
+++ ingest/autogenerateRedirects.py
@@ -21,7 +21,10 @@ def redirectUnit(FROM, TO):
   to={TO}
 """
 	return (unit)
-def combineDicts(dict1, dict2):
+def combineDictsJU(dict1, dict2):
+    """
+    Combine two Irreconcilable dictionaries, if they are not Irreconcilable, raising error.
+    """
     new_dict = {}
     for key in dict1:
         if key in new_dict:
@@ -36,6 +39,17 @@ def combineDicts(dict1, dict2):
 
     return (new_dict)
 
+
+def combineDictsOverwrite(dict1, dict2):
+    """
+    Combine two dictionaries and overwrite common keys of d1, d2 based on keys, values.
+    """
+    new_dict = dict1.copy()
+    for key in dict2:
+        new_dict[key] = dict2[key]
+    return (new_dict)
+
+
 def reductTonewLearnPathFromGHLinksCorrelation(inputMatrix):
     """
     This function takes as an argument our Matrix of the Ingest process and creates a new dictionary with key value
@@ -47,6 +61,20 @@ def reductTonewLearnPathFromGHLinksCorrelation(inputMatrix):
         outputDictionary[inputMatrix[x]["metadata"]["custom_edit_url"].replace("/edit/","/blob/")] = inputMatrix[x]["newLearnPath"]
     return (outputDictionary)
 
+
+def readRawStaticRedirectsFromFile(pathToFile):
+	"""
+	This function reads the netlify.toml file, identifies
+	the static section between # section: static START|END
+	and saves them into a multiline string.
+	"""
+	redirects = ""
+	section_pattern = re.compile(r'#\s*section:\s*static\s*<<\s*START(.+?)#\s*section:\s*static\s*<<\s*END', re.DOTALL)
+	with open(pathToFile, "r+") as fd:
+		document_text = "".join(fd.readlines())
+		sections = section_pattern.findall(document_text)
+		redirects += "".join(sections)
+	return (redirects)
 def readRedirectsFromFile(pathToFile):
 	"""
 	This function reads the netlify.toml file, identifies
@@ -65,6 +93,7 @@ def readRedirectsFromFile(pathToFile):
 		for k,v in redirectsList.__iter__():
 			redirects[k] = v
 	return (redirects)
+
 
 def readLegacyLearnDocMap(pathToFile):
 	"""
@@ -92,17 +121,31 @@ def main(GHLinksCorrelation):
 	oldLearn_redirects = UpdateGHLinksBasedOnMap(maping, oldLearn)
 	#print(oldLearn)
 	try:
-		finalDict = combineDicts(readRedirectsFromFile("netlify.toml"), oldLearn_redirects)
+		finalDict = combineDictsJU(readRedirectsFromFile("netlify.toml"), oldLearn_redirects)
 	except Exception as e:
 		print(f"An exception occurred: {e}")
-	unPackedDocument = ''
+	unPackedDynamicPart = ''
 	for key, value in finalDict.items():
 		if not value.startswith("https://"):
-			unPackedDocument+=redirectUnit(key, value)
-	
+			unPackedDynamicPart+= redirectUnit(key, value)
+	#print(unPackedDocument)
+	print(readRawStaticRedirectsFromFile("netlify.toml"))
 	print("Links from the legacy learn that are not matched:")
 	for key, value in finalDict.items():
 		if value.startswith("https://"):
 			print(key, value)
+	unPackedStaticPart= readRawStaticRedirectsFromFile("netlify.toml")
+	outputRedirectsFile = f"""
+# section: static << START
+{unPackedStaticPart}
+# section: static << END
+
+# section: dynamic << START
+{unPackedDynamicPart}
+# section: dynamic << END
+"""
+	f = open("netlify.toml", "w")
+	f.write(outputRedirectsFile)
+	f.close()
 	#print(unPackedDocument)
 	
