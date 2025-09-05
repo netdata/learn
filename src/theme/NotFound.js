4b8e9a4b179658d2671589d4242508410e1c07f4
fotis <fotis@netdata.cloud>
2025-09-05 10:26:35 +0300

    old 404 implementation

---
+++ src/theme/NotFound.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+
+export default function NotFound() {
+  useEffect(() => {
+    window.posthog.capture('page-not-found');
+    var url = location.href
+
+    var [first, second, three, ...query] = url.split('/')
+
+    query = query.toString().replaceAll(",", " ")
+    var target = query.replaceAll("\/", " ").replaceAll("#", " ").replaceAll("-", " ")
+    var base = window.location.origin
+    var redirectLocation = base + '?q=' + target
+
+    // Page location and redirectLocation should not be the same
+    if (location.href !== redirectLocation) {
+      // Redirect logic
+      window.location.href = redirectLocation;
+    }
+
+  }, [])
+  return null
+} 
